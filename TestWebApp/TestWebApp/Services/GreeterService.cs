ccc34aaf62dcd2b98b242d7cc50b899df15e3c26
Valery Yegorov <valery.yegorov@mobiline.com>
2026-01-08 06:24:27 -0500

    commit

---
+++ TestWebApp/TestWebApp/Services/GreeterService.cs
@@ -0,0 +1,16 @@
+using Contracts;
+using Grpc.Core;
+
+namespace TestWebApp.Services;
+
+public class GreeterService(ILogger<GreeterService> logger, IBaseRepository baseRepository)
+    : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger = logger;
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        
+        return Task.FromResult(baseRepository.GetHelloReply(request.Name));
+    }
+}
